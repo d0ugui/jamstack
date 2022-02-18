1e0035950a48fa34f493acb549dde24ad58f4286
Douglas Oliveira <douglaspo_97@outlook.com>
2022-02-18 12:09:06 -0300

    feat: create components and data fetching

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,23 +1,30 @@
 import { GetServerSideProps } from "next";
 import { usePageQuery, PageDocument } from "../generated/graphql";
+
+import { Hero } from "../components/Hero";
+import { Features } from "../components/Features";
+import { Pricing } from "../components/Princing";
+
 import { client, ssrCache } from '../lib/urql';
 
 export default function Home() {
   const [{ data }] = usePageQuery({
     variables: {
-      slug: 'about'
+      slug: 'home'
     }
   })
 
   return (
     <div>
-      <h1>{data?.page.title}</h1>
+      <Hero title={data.page.title} subtitle={data.page.subtitle} />
+      <Features />
+      <Pricing />
     </div>
   )
 }
 
 export const getServerSideProps: GetServerSideProps = async () => {
-  await client.query(PageDocument, { slug: 'about' }).toPromise();
+  await client.query(PageDocument, { slug: 'home' }).toPromise();
 
   return {
     props: {
